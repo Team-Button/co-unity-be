fcf3eea20d1499072eda8563ee3636d296bc6482
squashgray <squashgray@gmail.com>
2020-06-17 09:49:48 -0700

    edits

--- api/api-router.js
+++ api/api-router.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 
 const authRouter = require("../auth/auth-router");
-const usersRouter = require("../users/users-router");
+const usersRouter = require("../router/users-router");
 
 router.use("/auth", authRouter);
-router.use("/users", usersRouter);
 
 router.get("/", (req, res) => {
   res.json({ api: "Server Online" });
